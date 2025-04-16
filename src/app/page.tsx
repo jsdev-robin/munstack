ffc716caa3a51a3dc56a34a81b7cb0ee8377ae99
Md Robin <jsdev.robin@gmail.com>
2025-04-16 12:32:52 +0600

    Fix ESlinkt warnings or errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,7 @@
-"use client";
-import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
-
 import React from "react";
-import { Draggable } from "./Draggable";
-import { Droppable } from "./Droppable";
 
-const Home = () => {
-  return (
-    <div className="p-4">
-      <table className="table-auto border border-gray-300 w-full">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="border px-4 py-2">Main Column 1</th>
-            <th className="border px-4 py-2">Main Column 2</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border px-4 py-2">Main Row 1</td>
-            <td className="border px-4 py-2">
-              {/* Nested Table */}
-              <table className="table-auto border border-gray-300 w-full">
-                <thead>
-                  <tr className="bg-gray-50">
-                    <th className="border px-3 py-1 text-sm">Nested Col 1</th>
-                    <th className="border px-3 py-1 text-sm">Nested Col 2</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="border px-3 py-1 text-sm">Nested Row 1</td>
-                    <td className="border px-3 py-1 text-sm">Info</td>
-                  </tr>
-                  <tr>
-                    <td className="border px-3 py-1 text-sm">Nested Row 2</td>
-                    <td className="border px-3 py-1 text-sm">More Info</td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+const page = () => {
+  return <div>page</div>;
 };
 
-export default Home;
+export default page;
